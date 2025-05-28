b3ddb8b740132de4bb2564d312f6f118e2051ab4
Dan Neumann <danrodneu@gmail.com>
2025-05-28 13:44:00 -0500

    Updates

--- assets/hello-world.desc-BOuS9ByK.js
+++
@@ -1,9 +0,0 @@
-import { t as template } from "./index-CDSoUQHr.js";
-var _tmpl$ = /* @__PURE__ */ template(`<div><p>This is a simple demo that shows how to use the <code>Sprite</code> component to display an image.`);
-function helloWorld_desc() {
-  return _tmpl$();
-}
-export {
-  helloWorld_desc as default
-};
-//# sourceMappingURL=hello-world.desc-BOuS9ByK.js.map
